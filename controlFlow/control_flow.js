efff1e36e532ea9899a58052621611d0e2b04950
Ali200223 <aliamin_2002@hotmail.com>
2025-12-28 12:33:35 -0500

    updated controlFlow exercise

---
+++ controlFlow/control_flow.js
@@ -0,0 +1,54 @@
+
+
+
+let userRole = "admin";
+let  accesslevel;
+let isLoggedIn = true;
+let userMessage; 
+let userType = "subscriber";
+let userCategory;
+
+
+if (userRole === "admin"){
+    accesslevel = "Full access granted";
+} else if ( userRole === "manager"){
+    accesslevel = "limited access granted";
+
+} else {
+    accesslevel = "No access granted";
+
+
+} 
+
+
+if (isLoggedIn) {
+    if (userRole === "admin"){
+        userMessage = "Welcome admin";
+    }else if ( userRole === "manager"){
+        userMessage= "Welcome Manager";
+
+    }else{
+        userMessage ="Welcome User";
+
+    }
+}else {
+    userMessage = "Please log in to access the system";
+}
+
+switch (userType) {
+    case "admin":
+        userCategory = "Administrator";
+        break;
+    case "manager":
+        userCategory = "Manager";
+        break;
+    case "subscriber":
+        userCategory = "Subscriber";
+        break;
+    default:
+        userCategory = "Unknown";
+}
+
+console.log("Access Level:", accesslevel);
+console.log("User Message:",userMessage);
+console.log("User Category:", userCategory);
